691fbb5b7fd7322207350498d9c2caf3a16b9ef2
Nikita Chormale <nikitachormale4242@gmail.com>
2026-04-02 12:42:55 +0530

    schema design

--- server/modules/BucketList.js
+++
@@ -1,5 +0,0 @@
-import { Schema,model } from "mongoose";
-
- const bucketlistSchema = new Schema({
-    name:{ type : String, required: true},
- })
---
+++ server/modules/user.js
@@ -0,0 +1,24 @@
+import { Schema,model } from "mongoose";
+
+ const UserSchema = new Schema({
+    name:{ type : String,
+       required: true,
+      unique:true,
+   },
+   mobile:{
+      type:String,
+   },
+   city:{
+      type:String,
+   },
+   country:{
+      type: String,
+   },
+   password:{
+      type:String,
+      required:true,
+   },
+ });
+ const user =model("user",UserSchema);
+
+ export default user;
