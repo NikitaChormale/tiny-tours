2d8cc1f5a68bade3134c26b36131acc28b1bfcd3
Nikita Chormale <nikitachormale4242@gmail.com>
2026-04-08 12:24:15 +0530

    add the jwt Token

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from'express';
 import cors from "cors";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken"
 import connectDB from './db.js';
 import User from'./modules/user.js';
 import bcrypt from "bcrypt";
 import { get } from 'mongoose';
+
 
 dotenv.config();
 
@@ -17,18 +19,29 @@ const PORT =  process.env.PORT || 8080;
  
 const getkeeper=(req,res,next ) =>{
   const {name,ismember}= req.body;
-  console.log(`welcome,${name}`);
+  console.log(`hello,${name}`);
+
 
  if(ismember){
   next();
- } else{
+ } 
+ else{
   return res.json({
     message: "Access denied."
   });
  }
 };
-
-const testController= (req,res) =>{
+  
+const  areyoud = (req,res,next) =>{
+  const {areyoud}= req.body;
+  if (areyoud){
+    return res.json({ message:"entry not allowed"})
+  }
+  else{
+next();
+  }
+};
+const shamsunder= (req,res) =>{
   console.log("inside the controller");
 
   const random =Math.round ( Math.random() *100);
@@ -36,7 +49,10 @@ const testController= (req,res) =>{
   res.json({message: "thank you", random });
 };
 
-app.post("/test", getkeeper,testController);
+app.post("/test", getkeeper, areyoud,shamsunder);
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost: port ${PORT}`);
@@ -148,16 +164,60 @@ data: savedUser,
     });
   }
   const ispasswordCorrect =bcrypt .compareSync (password ,existingUser.password);
-   
+    existingUser.password=undefined; //hide password in response
 
   if(ispasswordCorrect){
+
+    const jwtToken = jwt.sign(
+      {
+        id:existingUser._id,
+        email: existingUser.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn:"1h",
+      }
+    );
     return res.json ({
       success:true,
       message: "login successfully",
       data: existingUser,
+      jwttoken: jwtToken ,
+      
     });
-  } 
+  }  else{
+    return res.json ({
+      success:false,
+      message: "Invalid email or password",
+      data: null,
+    });
+  }
 });
+
+const checkjwt= (req,res,next) =>{
+   const { authorization} =req.headers;
+  const Token =authorization && authorization.split (" ")[1];
+
+  try {
+    const decoded= jwt.verify(Token,proccess.env.JWT_SECRET);
+    next();
+   }
+   catch(err){
+    return res.json({
+      success:true,
+    message:" Invalid or missing token",
+    data:null  
+  });
+   }
+  }
+   
+app.get("/api_v1", checkjwt ,(req,res)=>{
+  res.json({message:"API v1 is working"});
+});
+app.get("/api_v2",(req,res)=>{
+res.json({ message:"API v2 is working "});
+});
+   
 
 
 
